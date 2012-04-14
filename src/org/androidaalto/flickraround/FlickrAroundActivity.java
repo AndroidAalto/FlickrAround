1ae6a87dcadd3e956fa43b62ea8196408331e2bb
Ramon <oni.pool@gmail.com>
2012-04-14 18:26:44 +0300

    Added button to fetch the current location

--- src/org/androidaalto/flickraround/FlickrAroundActivity.java
+++ src/org/androidaalto/flickraround/FlickrAroundActivity.java
@@ -1,7 +1,13 @@
+
 package org.androidaalto.flickraround;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 
 public class FlickrAroundActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,52 @@ public class FlickrAroundActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /**
+     * 
+     */
+    private void getCurrentLocationAndFetchPics() {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location
+                // provider.
+                fetchFlickrPicsByLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location
+        // updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                locationListener);
+    }
+
+    /**
+     * @param location
+     */
+    protected void fetchFlickrPicsByLocation(Location location) {
+        System.out.println("Latitude: " + location.getLatitude());
+        System.out.println("Longitude: " + location.getLongitude());
+    }
+
+    /**
+     * @param location
+     */
+    public void fetchFlickrPics(View view) {
+        getCurrentLocationAndFetchPics();
     }
 }
