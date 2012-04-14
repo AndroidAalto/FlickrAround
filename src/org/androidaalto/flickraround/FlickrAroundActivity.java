6ca754d2fbaac4a94ff68f6d7fbc5028ef116f57
Ramon <oni.pool@gmail.com>
2012-04-14 17:52:21 +0300

    First commit

---
+++ src/org/androidaalto/flickraround/FlickrAroundActivity.java
@@ -0,0 +1,13 @@
+package org.androidaalto.flickraround;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlickrAroundActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
